f1707dcc3f8aacffc0932310e8debb040a74422f
Kohei Aoki <aoki@qri.jp>
2015-02-25 14:28:51 +0900

    refactoring

--- backboneArray.js
+++ backboneArray.js
@@ -1,36 +1,50 @@
-(function(Backbone){
-    Backbone.Object = function(obj)
-    {
-        this.obj;
+/**
+ * Created by aoki on 9/22/14.
+ */
 
-        _.extend(this,Backbone.Events);
+(function (Backbone) {
+    "use strict";
 
-        this.length = function()
-        {
-            return _.size(this.obj);
-        }
+    //private method
+    function fireEvent(self, e, changeEvent) {
+        self.trigger(changeEvent, e);
+        self.trigger("change", e);
 
-        this.init  = function(obj)
-        {
-            var e;
-            this.obj = (obj) ? obj: {};
-            e = setEvent(this,obj,obj);
-            fireEvent(this,e,"reset");
-            return this.obj;
-        }
+    }
 
-
-        this.set = function (json)
-        {
-            var e;
-            addJson(this.obj,json);
-            e = setEvent(this,this.obj,json);
-            fireEvent(this,e,"add");
-            return this.obj;
+    function setEvent(self, data, changeData) {
+        var e = {
+            self: null,
+            data: null
         };
-        function addJson(obj,json)
-        {
-            var keys= [],
+        if (self) e.self = self;
+        if (data) e.data = data;
+        if (changeData) e.changeData = changeData;
+
+        return e;
+    }
+
+    /**
+     * @class add,remove時にイベントを発火するモデル
+     * @param obj
+     * @constructor
+     */
+    Backbone.EventObject = function (obj) {
+        var _that = this;
+        this.attr;
+
+
+        //private method
+        var _init = function (obj) {
+            var e;
+            _that.attr = _.clone(obj) ? obj : {};
+            e = setEvent(_that, obj, obj);
+            fireEvent(_that, e, "reset");
+            return _that.attr;
+        };
+
+        function addJson(obj, json) {
+            var keys = [],
                 i = 0,
                 jsonLength,
                 key;
@@ -38,120 +52,285 @@
                 keys = getKeyFromJson(json);
                 jsonLength = keys.length;
 
-                for (i;i<jsonLength;i++)
-                {
+                for (i; i < jsonLength; i++) {
                     key = keys[i];
                     obj[key] = json[key];
                 }
                 return obj;
-            }catch (e)
-            {
-                console.log("e",e);
+            } catch (e) {
+                console.log("e", e);
                 return false;
             }
         }
-        function getKeyFromJson(json)
-        {
-            var key,keys=[];
+
+        function getKeyFromJson(json) {
+            var key, keys = [];
             try {
-                for (key in json)
-                {
+                for (key in json) {
                     keys.push(key);
                 }
-            }catch (e){
-                console.log("e",e);
+            } catch (e) {
+                console.log("e", e);
                 return false;
             }
             return keys;
         };
 
-//        this.pop = function(){
-//            this.array.pop();
-//        }
-//
-//        this.swap = function(indexA,indexB)
-//        {
-//            var indexA = this.array[indexA];
-//            var indexB = this.array[indexB];
-//
-//            if (indexA && indexB)
-//            {
-//                this.array[indexA] = indexB;
-//                this.array[indexB] = indexA;
-//            }
-//            this.trigger("change",this);
-//            return this;
-//        }
 
-        this.init(obj);
+        _init(obj);
 
     };
 
-    Backbone.Array = function(Array)
-    {
-        var self = this;
-        this.array;
-        _.extend(this,Backbone.Events);
+    var bbEventsObject = {};
 
-        this.init  = function(Array)
-        {
-            this.array = (Array) ? Array: [];
-            return this.array;
-        }
+    _.extend(bbEventsObject, Backbone.Events);
+
+    //public
+    bbEventsObject.length = function () {
+        return _.size(this.attr);
+    };
 
 
-        this.push = function (data)
-        {
-            var e ;
-            this.array.push(data);
-            e = setEvent(this,this.array,data);
-            fireEvent(this,e,"add");
+    bbEventsObject.get = function (key) {
+        return this.attr[key];
 
-            return this.array;
-        };
+    };
 
-        this.pop = function(){
-            var e;
-            this.array.pop();
-            e = setEvent(this,this.array);
-            fireEvent(this,e,"delete");
-            return this.array;
-        }
+    bbEventsObject.unset = function (key) {
+        var e;
 
-        this.swap = function(indexA,indexB)
-        {
-            var e;
-            var indexA = this.array[indexA];
-            var indexB = this.array[indexB];
+        if (!key) return false;
 
-            if (indexA && indexB)
-            {
-                this.array[indexA] = indexB;
-                this.array[indexB] = indexA;
+        delete this.attr[key];
+
+
+
+        e = setEvent(this, this.attr, null);
+        fireEvent(this, e, "remove");
+        return this.attr;
+    };
+
+    bbEventsObject.set = function (key, value) {
+        var e;
+
+        if (!key||!value) return false;
+
+        this.attr[key] = value;
+        e = setEvent(this, this.attr, value);
+        fireEvent(this, e, "add");
+        return this.attr;
+    };
+
+    bbEventsObject.setObj = function (value) {
+        var e;
+
+        if (!value) return false;
+
+        _.extend(this.attr,value);
+        e = setEvent(this, this.attr, value);
+        fireEvent(this, e, "add");
+        return this.attr;
+    };
+
+
+    bbEventsObject.reset = function (json) {
+        var e;
+        this.attr = json ? _.clone(json) : {};
+        e = setEvent(this, this.attr, json);
+        fireEvent(this, e, "reset");
+        return this.attr;
+    };
+
+    bbEventsObject.each = function (iter) {
+        _.each(this.attr, iter);
+    };
+
+
+    bbEventsObject.toJSON = function () {
+        var json = {};
+        for (var key in this.attr) {
+            if (this.attr[key]) {
+                if (this.attr[key]["toJSON"] !== undefined) {
+                    json[key] = this.attr[key].toJSON();
+                } else {
+                    json[key] = this.attr[key];
+
+                }
             }
-
-            e = setEvent(this,this.array,[indexA,indexB]);
-            fireEvent(this,e,"swap");
-            return this.array;
         }
 
-        this.init(Array);
+        return json;
+    };
+
+    bbEventsObject.clear = function () {
+        this.attr = {};
+        return this.attr;
+    };
+
+    Backbone.EventObject.prototype = bbEventsObject;
+
+
+
+    var _bbArrayInit = function (array) {
+
+        if (array && array instanceof Array){
+            return cloneArray(array);
+        }else if (array && !(array instanceof Array)){
+            if (window.console){
+                console.log("Invalid param");
+            }
+            return [];
+        }else{
+            return [];
+        }
+    }
+
+    /**
+     * @param Array
+     * @constructor
+     */
+    Backbone.Array = function (Array) {
+        var _that = this;
+        this.array;
+
+
+        this.array = _bbArrayInit(Array);
+    };
+
+    var bbArray = {};
+    _.extend(bbArray, Backbone.Events);
+
+    /**
+     * @returns {number}
+     */
+    bbArray.length = function () {
+        return _.size(this.array);
+    };
+
+    bbArray.reset = function (array) {
+        var e;
+
+        this.array = _bbArrayInit(array);
+
+        e = setEvent(this, this.array, array);
+        fireEvent(this, e, "reset");
+        return this.array;
+    };
+
+    bbArray.set = function (index, data) {
+        var e;
+        if (!this.array[index]) {
+            return false;
+        }
+        this.array[index] = data;
+        e = setEvent(this, this.array, data);
+        fireEvent(this, e, "update");
+        return this.array;
+    };
+
+    bbArray.splice = function (begin, end) {
+        var e;
+        var data;
+        var begin = begin, end = end;
+
+        (end) || (end = begin);
+
+        data = this.array.splice(begin, end);
+        e = setEvent(this, this.array, data);
+        fireEvent(this, e, "delete");
+        return this.array;
+    };
+
+    bbArray.push = function (data) {
+        var e;
+        this.array.push(data);
+        e = setEvent(this, this.array, data);
+        fireEvent(this, e, "add");
+
+        return this.array;
+    };
+
+    bbArray.get = function (index) {
+        return this.array[index] || undefined;
+    };
+
+    bbArray.pop = function () {
+        var e;
+        this.array.pop();
+        e = setEvent(this, this.array);
+        fireEvent(this, e, "delete");
+        return this.array;
+    };
+
+    bbArray.each = function (iter) {
+
+        if (!iter) return false;
+
+        _.each(this.array, iter);
+    };
+
+    bbArray.swap = function (indexA, indexB) {
+        var e;
+        var itemA = this.array[indexA];
+        var itemB = this.array[indexB];
+
+        if (!itemA || !itemB) return false;
+
+        this.array[indexA] = itemB;
+        this.array[indexB] = itemA;
+
+        e = setEvent(this, this.array, [indexA, indexB]);
+        fireEvent(this, e, "swap");
+        return this.array;
+    };
+
+    bbArray.toJSON = function () {
+        var json = [];
+        var i = 0;
+
+        for (i; i < this.array.length; i++) {
+
+            if (this.array[i]["toJSON"] !== undefined) {
+                json.push(this.array[i].toJSON());
+            } else {
+                json.push(this.array[i]);
+            }
+        }
+        return json;
+    };
+
+    bbArray.getRowArray = function () {
+        return this.array;
     };
 
     //private method
-    function fireEvent(self,e,changeEvents){
-        _.each(changeEvents,function(i,item){
-            (item)||self.trigger(item,e);
-        });
-        self.trigger("change",e);
+    function fireEvent(self, e, changeEvent) {
+        self.trigger(changeEvent, e);
+        self.trigger("change", e);
 
     }
 
-    function setEvent(self,data,changeData){
-        var e;
-        (self)|| (e.self = self);
-        (data)||(e.data = data);
-        (changeData)||(e.changeData = changeData);
+    Backbone.Array.prototype = bbArray;
+
+
+    function cloneArray(arr) {
+        var newarr = [];
+        for (var i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                newarr[i] = cloneArray(arr[i]);
+            } else {
+                newarr[i] = _.clone(arr[i]);
+            }
+        }
+        return newarr;
+    }
+
+    function setEvent(self, data, changeData) {
+        var e = {};
+        if (self) e.self = self;
+        if (data) e.data = data;
+        if (changeData) e.changeData = changeData;
+
         return e;
     }
 }(Backbone));
